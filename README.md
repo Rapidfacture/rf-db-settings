553fb0e930702b0b99892ddfcf692936493147a2
Felix <ff@rapidfacture.com>
2018-01-31 23:58:16 +0100

    add files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rf-db
 Rapifacture database module. Provides mongoose, Gridfs and Db settings
+
+* tries to use 'rf-log'
+
+
+##
+
+>npm install rf-db
+
+```js
+var db = require('rf-db').start({
+   db: db,
+   pathsSchemas: 'dest',
+   settings: {
+
+   }
+});
+
+// the webserver is running on port 3000 using folder 'dest' as root
+// user express 'app' for further things like
+// adding middleware, websockets, etc.
+```
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // rf-db
 
+var mongooseMulti = require('mongoose-multi');
+
 // logging
 var log = {
    info: console.log,
    error: console.error,
    critical: function () {
-      throw new Error(console.error.apply(arguments))
+      throw new Error(console.error.apply(arguments));
    }
 };
 try { // try using rf-log
@@ -13,89 +15,72 @@ try { // try using rf-log
 } catch (e) {}
 
 
-var mongooseMulti = require('mongoose-multi'),
-
-
-
 
 module.exports.start = function (options, callback) {
-
-
    if (!options.db) {
-      log.critical("no db configuration specified ", options.db)
+      log.critical('no db configuration specified ', options.db);
    }
-   if (!options.pathsSchemas){
-       log.critical("no db schemas specified ", options.pathsSchemas)
+   if (!options.pathsSchemas) {
+      log.critical('no db schemas specified ', options.pathsSchemas);
    }
 
    var db = mongooseMulti.start(options.db, options.pathsSchemas);
 
+
    db.global.mongooseConnection.once('open', function () {
 
+      if (options.settings) {
+         // var settings = [
+         //    {
+         //       name: 'globalSettings',
+         //       query: 'global'
+         //    },
+         //    {
+         //       name: 'appSettings',
+         //       query: config.app.name
+         //    },
+         //    {
+         //       name: 'mailSettings',
+         //       query: 'mail'
+         //    }
+         // ];
+         var settings = options.settings;
+         getNextSetting();
+      }
 
-  // TODO: create a function that gets a list of setting files to fetch and returns the settings
-
-      var settings= [
-         {
-            name: 'globalSettings',
-            query: 'global'
-         },
-         {
-            name: 'appSettings',
-            query: config.app.name
-         },
-         {
-            name: 'mailSettings',
-            query: 'mail'
-         },
-      ];
-      
       // how can this parameter be passed back?
       var config = {};
 
       var counter = 0;
-
-      function getNextSetting(){
+      function getNextSetting () {
          var name = settings[counter];
          var query = settings[counter];
-         getGlobalSettings(query, function setGlobalSettings (setting) {
+         getGlobalSettings(query, function (setting) {
             counter++;
-            config.[name] = setting;
+            config[name] = setting;
             if (counter < settings.length) getNextSetting();
          });
       }
-      
-      getNextSetting()
 
 
-      // Load settings
-      getGlobalSettings('global', function setGlobalSettings (globalSettings) {
-         config.global = globalSettings;
-         getGlobalSettings(config.app.name, function setGlobalSettings (appSettings) {
-            config.appSettings = appSettings;
-            callback(db);
-         });
-      });
-      
-   });
-   
+      function getGlobalSettings (name, callback) {
+         db.global.settings
+            .findOne({
+               'name': name
+            })
+            .exec(function (err, doc) {
+               if (doc && doc.settings) {
+                  callback(doc.settings);
+               } else if (err) {
+                  log.critical(err);
+               } else {
+                  log.critical('no ' + name + ' settings found in DB global');
+               }
+            });
+      }
+
+
    return db;
 };
 
-
-
-function getGlobalSettings (name, callback) {
-   db.global.settings
-      .findOne({
-         'name': name
-      })
-      .exec(function (err, doc) {
-         if (doc && doc.settings) {
-            callback(doc.settings);
-         } else if (err) {
-            log.critical(err);
-         } else {
-            log.critical('no ' + name + ' settings found in DB global');
-         }
-      });
 }
